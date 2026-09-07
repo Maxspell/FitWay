c38457039da33f123b9ae87afcb8aad4aaa0af66
Maxspell <m.stizhko@gmail.com>
2026-09-07 17:16:09 +0300

    feat: add robots.txt configuration for site indexing and crawler instructions

--- frontend/src/app/robots.ts
+++ frontend/src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/api/', '/*?*_rsc=*'],
+      disallow: ['/api/', '/*?*_rsc=*', '/_next/'],
     },
     sitemap: 'https://fitway.best/sitemap.xml',
   };
